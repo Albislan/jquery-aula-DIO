6cd55d7f3c23745740ea6237f23c4783e24778d6
Albislan <64960600+Albislan@users.noreply.github.com>
2020-09-04 09:51:00 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# jquery-aula-DIO
---
+++ main.js
@@ -0,0 +1,16 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/"
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+        }
+    });
+}
